94468169026d0f8aeaac30edf03be510aa049036
hupe1980 <frankhuebner1980@gmail.com>
2019-12-02 22:47:50 +0100

    feat: Add leading zeros

--- frontend/src/components/time-to-live.tsx
+++ frontend/src/components/time-to-live.tsx
@@ -4,6 +4,13 @@ import Chip from '@material-ui/core/Chip';
 export interface TimeToLiveProps {
   expirationTime: number;
 }
+const addLeadingZeros = (value: number) => {
+  let stringValue = String(value);
+  while (stringValue.length < 2) {
+    stringValue = '0' + stringValue;
+  }
+  return stringValue;
+};
 
 export const TimeToLive: React.FC<TimeToLiveProps> = ({ expirationTime }) => {
   const [countDown, setCountDown] = useState('');
@@ -14,12 +21,14 @@ export const TimeToLive: React.FC<TimeToLiveProps> = ({ expirationTime }) => {
 
       const distance = expirationTime - now;
 
-      const days = Math.floor(distance / (60 * 60 * 24));
-      const hours = Math.floor((distance % (60 * 60 * 24)) / (60 * 60));
-      const minutes = Math.floor((distance % (60 * 60)) / 60);
-      const seconds = Math.floor(distance % 60);
+      const days = addLeadingZeros(Math.floor(distance / (60 * 60 * 24)));
+      const hours = addLeadingZeros(
+        Math.floor((distance % (60 * 60 * 24)) / (60 * 60))
+      );
+      const minutes = addLeadingZeros(Math.floor((distance % (60 * 60)) / 60));
+      const seconds = addLeadingZeros(Math.floor(distance % 60));
 
-      setCountDown(`${days}d ${hours} h ${minutes} m ${seconds} s`);
+      setCountDown(`${days} : ${hours} : ${minutes} : ${seconds}`);
 
       if (distance < 0) {
         setCountDown('EXPIRED');
